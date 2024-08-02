dcab8f2935117fee18131012fbca6a74f7d78e77
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-08-02 12:05:52 -0300

    Velocidad al JugadorMain

---
+++ Assets/Scripts/ScriptJugadorMain.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptJugadorMain : MonoBehaviour
+{
+    public float Velocidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcesarMovimiento();
+    }
+
+    void ProcesarMovimiento()
+    {
+
+        float inputVelocidad = Input.GetAxis("Horizontal");
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = new Vector2(inputVelocidad * Velocidad, rigidbody.velocity.y);
+    }
+}
