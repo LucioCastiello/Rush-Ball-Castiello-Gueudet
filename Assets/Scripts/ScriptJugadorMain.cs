a1af2635f1eb29898ee72221c50807ccf5756123
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-08-06 14:05:24 -0300

    terminado el salto

--- Assets/Scripts/ScriptJugadorMain.cs
+++ Assets/Scripts/ScriptJugadorMain.cs
@@ -6,10 +6,13 @@ public class ScriptJugadorMain : MonoBehaviour
 {
     public float Velocidad;
     public float Salto;
+    private BoxCollider2D boxcollider;
+    public LayerMask CapaPasto;
 
     // Start is called before the first frame update
     void Start()
     {
+        boxcollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -27,9 +30,23 @@ public class ScriptJugadorMain : MonoBehaviour
         rigidbody.velocity = new Vector2(inputVelocidad * Velocidad, rigidbody.velocity.y);
     }
 
+    bool EstaEnSuelo()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(
+            boxcollider.bounds.center,
+            boxcollider.bounds.size,
+            0f,
+            Vector2.down,
+            0.1f, // Distancia del BoxCast, ajusta según sea necesario
+            CapaPasto
+        );
+
+        return hit.collider != null;
+    }
+
     void ProcesarSalto()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && EstaEnSuelo())
         {
             Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
             rigidbody.AddForce(Vector2.up * Salto, ForceMode2D.Impulse);
