f9873ee9127a2a352e385a67928544b2fffe5ebf
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-08-06 11:59:11 -0300

    +Salto +Sefguimiento de camara., Falta arreglar doble salto

--- Assets/Scripts/ScriptJugadorMain.cs
+++ Assets/Scripts/ScriptJugadorMain.cs
@@ -1,26 +1,26 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScriptJugadorMain : MonoBehaviour
 {
     public float Velocidad;
+    public float Salto;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcesarMovimiento();
+        ProcesarSalto();
     }
 
     void ProcesarMovimiento()
     {
-
         float inputVelocidad = Input.GetAxis("Horizontal");
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
@@ -29,6 +29,10 @@ public class ScriptJugadorMain : MonoBehaviour
 
     void ProcesarSalto()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+            rigidbody.AddForce(Vector2.up * Salto, ForceMode2D.Impulse);
+        }
     }
 }
