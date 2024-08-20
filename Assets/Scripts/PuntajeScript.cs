27b65d4b7b434804be69e27c697bd4c97ecf58ac
CASTIELLO LUCIO <47873245@ort>
2024-08-20 14:28:29 -0300

    Inicio puntraje

---
+++ Assets/Scripts/PuntajeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PuntajeScript : MonoBehaviour
+{
+    private float puntos;
+    private TextMeshProUGUI textmesh;
+
+    private void Start()
+    {
+        textmesh = GetComponent<TextMeshProUGUI>();
+    }
+    private void Update()
+    {
+        puntos += Time.deltaTime;
+        
+    }
+}
