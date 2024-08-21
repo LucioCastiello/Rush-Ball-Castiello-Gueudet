f5c68e0585a985cf2e9af1166f499104413914ec
LUNA GUEUDET JUAN MANUEL <48184278@ort>
2024-08-21 08:54:36 -0300

    destructor obstaculo

---
+++ Assets/Scripts/controladorobstaculo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorobstaculo : MonoBehaviour
+
+    
+{
+
+    public float velocidad = 2f;
+
+    private Rigidbody2D rigid2d;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid2d = GetComponent<Rigidbody2D>();
+        rigid2d.velocity = Vector2.left * velocidad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Destroyer")
+        {
+            Destroy(gameObject);
+        }
+       
+    }
+}
