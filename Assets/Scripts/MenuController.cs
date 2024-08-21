c451828c3ebcae6e6d8213f435b3a73474a750b3
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-08-21 15:36:57 -0300

    Arregle error de obstaculo y manejo de escenas

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    // Asigna el botón desde el inspector
+    public Button jugarButton;
+
+    void Start()
+    {
+        // Asegúrate de que el botón esté asignado
+        if (jugarButton != null)
+        {
+            jugarButton.onClick.AddListener(CargarDribbleDash);
+        }
+    }
+
+    void CargarDribbleDash()
+    {
+        // Cambia a la escena "DribbleDash"
+        SceneManager.LoadScene("Dribble Dash");
+    }
+}
