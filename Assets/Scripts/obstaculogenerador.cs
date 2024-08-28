96c050095b9943c903625f75babd781f97e42b11
LUNA GUEUDET JUAN MANUEL <48184278@ort>
2024-08-28 08:37:57 -0300

    generador obstaculos

---
+++ Assets/Scripts/obstaculogenerador.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstaculogenerador : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    public float generatorTimer = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", 0f, generatorTimer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void CreateObstacle()
+    {
+        Instantiate(obstaclePrefab, transform.position, Quaternion.identity);
+    }
+}
