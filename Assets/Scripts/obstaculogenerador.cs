3d8eb141fafec05c73d2c3b5bd43e3bebf26ec83
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-08-30 17:56:36 -0300

    cambie obstaculo por defensor

--- Assets/Scripts/obstaculogenerador.cs
+++ Assets/Scripts/obstaculogenerador.cs
@@ -10,7 +10,7 @@ public class obstaculogenerador : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("CreateObstacle", 0f, generatorTimer);
+        InvokeRepeating("CreateObstacle", 1f, generatorTimer);
     }
 
     // Update is called once per frame
