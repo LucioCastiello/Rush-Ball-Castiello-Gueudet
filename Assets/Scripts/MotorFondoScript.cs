84610ceb07ba5b9b2df035f830f7c812cd654d9b
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-09-20 11:59:51 -0300

    Arranque Scroll fondo infinito,

---
+++ Assets/Scripts/MotorFondoScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorFondoScript : MonoBehaviour
+{
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Speed * Time.deltaTime, 0, 0); 
+    }
+}
