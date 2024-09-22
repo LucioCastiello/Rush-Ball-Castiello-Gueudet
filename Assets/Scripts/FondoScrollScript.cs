c844cfb63668183754dddfd463be3341b5f6cd36
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-09-22 17:24:29 -0300

    Falta arreglar un error de scroll

---
+++ Assets/Scripts/FondoScrollScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FondoScrollScript : MonoBehaviour
+{
+
+    public float displacementAmount;
+  
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("ZonaDesplacamiento"))
+        {
+            transform.position += new Vector3(displacementAmount, 0, 0);
+        }
+    }
+   
+}
