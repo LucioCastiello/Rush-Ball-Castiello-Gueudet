24f3b5c66e29cf92892bedd3c4c08ccc5f13c040
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-10-14 14:16:04 -0300

    Termine credits

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject panel;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,15 @@ public class MenuScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ActivarPanel()
+    {
+        panel.SetActive(true);  // Activa el panel
+    }
+
+    public void DesactivarPanel()
+    {
+        panel.SetActive(false);  // Desactiva el panel
     }
 }
