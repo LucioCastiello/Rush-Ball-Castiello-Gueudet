f7259f2da8256b60641243ac8e0fd7a50cee7ccd
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-10-31 14:44:11 -0300

    Menupausa

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+
+    [SerializeField] private GameObject botonpausa;
+    [SerializeField] private GameObject menupausa;
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+        botonpausa.SetActive(false);
+        menupausa.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        botonpausa.SetActive(true);
+        menupausa.SetActive(false);
+    }
+
+   
+}
