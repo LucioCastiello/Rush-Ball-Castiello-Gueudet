72d56e9bd6f544dc4a7cc841dd6efe850e4cb659
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-11-07 15:43:06 -0300

    Last time, animaciones en botones. ARRIBA Q ESTO NO CAE, VAMOS CHACARITA Y RUSH BALL

---
+++ Assets/UiPuntosWinkel.cs
@@ -0,0 +1,21 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UiPuntosWinkel : MonoBehaviour
+{
+    public TextMeshProUGUI txt_tusPuntos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt_tusPuntos.text = GAMEMANAGER.Instance.puntos.ToString("F1"); 
+    }
+}
